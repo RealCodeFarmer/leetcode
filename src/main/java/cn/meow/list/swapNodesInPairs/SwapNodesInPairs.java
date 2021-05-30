bbd903fcb4cb7685ac89b7ba1f4417391a6392c4
yushuhao <1241515599@qq.com>
2021-05-30 17:10:51 +0800

    Swap Nodes in Pairs (Medium)

---
+++ src/main/java/cn/meow/list/swapNodesInPairs/SwapNodesInPairs.java
@@ -0,0 +1,22 @@
+package cn.meow.list.swapNodesInPairs;
+
+public class SwapNodesInPairs {
+
+    /**
+     * 递归
+     * s -> f-> swapPairs(ListNode)
+     * f-> swapPairs(ListNode) <- s
+     * f -> s -> swapPairs(ListNode)
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        slow.next = fast.next;
+        fast.next = slow;
+        slow.next = swapPairs(slow.next);
+        return fast;
+    }
+}
