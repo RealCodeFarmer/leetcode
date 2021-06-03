b82d3992370430ec2810a9bd3516d247b2a92917
yushuhao <1241515599@qq.com>
2021-06-03 23:07:49 +0800

    Reverse Vowels of a String & Valid Palindrome II

---
+++ src/main/java/cn/meow/doublepointer/validPalindromeII/ValidPalindromeII.java
@@ -0,0 +1,36 @@
+package cn.meow.doublepointer.validPalindromeII;
+
+public class ValidPalindromeII {
+
+    /**
+     * 双指针
+     * 如果不相等,要么就是head前进1,要么就是tail后退1
+     */
+    public boolean validPalindrome(String s) {
+        int head = 0;
+        int tail = s.length() - 1;
+        while (tail > head) {
+            if (s.charAt(head) != s.charAt(tail)) {
+                return isPalindrome(s, head + 1, tail) || isPalindrome(s, head, tail - 1);
+            }
+            head++;
+            tail--;
+        }
+        return true;
+    }
+
+    /**
+     * 双指针
+     * 判断是否回文字符串
+     */
+    private boolean isPalindrome(String s, int head, int tail) {
+        while (tail > head) {
+            if (s.charAt(head) != s.charAt(tail)) {
+                return false;
+            }
+            head++;
+            tail--;
+        }
+        return true;
+    }
+}
