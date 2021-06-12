17157a4bea6aa23de788a7186d3b8fc6494f2476
yushuhao <1241515599@qq.com>
2021-06-12 23:24:34 +0800

    Non-decreasing Array & Maximum Subarray

---
+++ src/main/java/cn/meow/greedyThinking/maximumSubarray/MaximumSubarray.java
@@ -0,0 +1,11 @@
+package cn.meow.greedyThinking.maximumSubarray;
+
+public class MaximumSubarray {
+
+    /**
+     * 感觉做过(浙大某课程), 但是没整出来
+     */
+    public int maxSubArray(int[] nums) {
+        return -1;
+    }
+}
