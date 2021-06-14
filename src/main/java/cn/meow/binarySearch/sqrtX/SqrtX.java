f5d7a170878f6d759492f8790c5c8349bb1c4ef5
yushuhao <1241515599@qq.com>
2021-06-14 17:28:31 +0800

    Sqrt(x)

---
+++ src/main/java/cn/meow/binarySearch/sqrtX/SqrtX.java
@@ -0,0 +1,22 @@
+package cn.meow.binarySearch.sqrtX;
+
+public class SqrtX {
+
+    /**
+     * 遍历
+     */
+    public int mySqrt(int x) {
+        int result = -1;
+        for (int i = 0; i < (x / 2) + 2; i++) {
+            int val = i * i;
+            //题意: 计算并返回 x 的平方根，其中 x 是非负整数
+            //为负数,肯定是正数溢出了
+            if (val >= 0 && val <= x) {
+                result = i;
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+}
