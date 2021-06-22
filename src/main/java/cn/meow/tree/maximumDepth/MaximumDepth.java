c877580b163d4e451fcd09928339dd92a662eb83
yushuhao <1241515599@qq.com>
2021-06-22 21:41:55 +0800

    Maximum Depth of Binary Tree & Balanced Binary Tree

---
+++ src/main/java/cn/meow/tree/maximumDepth/MaximumDepth.java
@@ -0,0 +1,17 @@
+package cn.meow.tree.maximumDepth;
+
+import cn.meow.tree.been.TreeNode;
+
+public class MaximumDepth {
+
+    /**
+     * 递归
+     * DFS
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
---
+++ src/main/java/cn/meow/tree/maximumDepth/MaximumDepth2.java
@@ -0,0 +1,35 @@
+package cn.meow.tree.maximumDepth;
+
+import cn.meow.tree.been.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepth2 {
+
+    /**
+     * 广度优先搜索
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+            result++;
+        }
+        return result;
+    }
+}
