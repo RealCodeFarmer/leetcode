b260b1644aff4c6bdb02a0b9400f3698ec536d60
yushuhao <1241515599@qq.com>
2021-06-24 20:39:20 +0800

    Merge Two Binary Trees & Path Sum

---
+++ src/main/java/cn/meow/tree/pathSum/PathSum.java
@@ -0,0 +1,77 @@
+package cn.meow.tree.pathSum;
+
+import cn.meow.tree.been.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSum {
+
+    private List<Integer> list = new ArrayList<>();
+
+    /**
+     * 递归
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        boolean result = false;
+        if (root == null) {
+            return result;
+        }
+        depth(root, 0);
+        if (list != null && !list.isEmpty()) {
+            for (Integer integer : list) {
+                if (integer.intValue() == targetSum) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    private void depth(TreeNode root, int preVal) {
+        root.val += preVal;
+        if (root.left != null) {
+            depth(root.left, root.val);
+        }
+        if (root.right != null) {
+            depth(root.right, root.val);
+        }
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+        }
+    }
+
+    class Solution {
+        private boolean result = false;
+        private int targetSum;
+
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            this.targetSum = targetSum;
+            if (root == null) {
+                return result;
+            }
+            depth(root, 0);
+            return result;
+        }
+
+        private void depth(TreeNode root, int preVal) {
+            if (result) {
+                return;
+            }
+            root.val += preVal;
+            if (root.left != null) {
+                depth(root.left, root.val);
+            }
+            if (root.right != null) {
+                depth(root.right, root.val);
+            }
+            if (!result && root.left == null && root.right == null) {
+                if (targetSum == root.val) {
+                    result = true;
+                    return;
+                }
+            }
+        }
+    }
+}
