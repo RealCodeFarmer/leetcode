4b34cd9b570fc6b84ff682ebc507b06e5054568b
yushuhao <1241515599@qq.com>
2021-06-29 21:26:43 +0800

    Symmetric Tree &  Minimum Depth of Binary Tree

---
+++ src/main/java/cn/meow/tree/symmetricTree/SymmetricTree2.java
@@ -0,0 +1,40 @@
+package cn.meow.tree.symmetricTree;
+
+import cn.meow.tree.been.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SymmetricTree2 {
+
+    /**
+     * 迭代
+     * 广度
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            queue.offer(t1.left);
+            queue.offer(t2.right);
+            queue.offer(t1.right);
+            queue.offer(t2.left);
+        }
+        return true;
+    }
+}
