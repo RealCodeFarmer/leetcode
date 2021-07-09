589ef0d4bf00a3898da4a1415b28371c2d22582f
yushuhao <1241515599@qq.com>
2021-07-10 00:48:43 +0800

    update

--- src/main/java/cn/meow/tree/postorderTraversal/PostorderTraversal.java
+++ src/main/java/cn/meow/tree/postorderTraversal/PostorderTraversal.java
@@ -4,6 +4,7 @@ import cn.meow.tree.been.TreeNode;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PostorderTraversal {
@@ -24,5 +25,33 @@ public class PostorderTraversal {
         dfs(root.left, res);
         dfs(root.right, res);
         res.add(root.val);
+    }
+
+    /**
+     * 迭代
+     * 队列
+     * 前序遍历为 root -> left -> right
+     * 后序遍历为 left -> right -> root
+     * 可以修改前序遍历成为 root -> right -> left, 那么这个顺序就和后序遍历正好相反
+     */
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        if (root == null) {
+            return Collections.EMPTY_LIST;
+        }
+        List<Integer> res = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            res.add(node.val);
+            if (node.left != null) {
+                queue.offerFirst(node.left);
+            }
+            if (node.right != null) {
+                queue.offerFirst(node.right);
+            }
+        }
+        Collections.reverse(res);
+        return res;
     }
 }
