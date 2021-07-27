222bec2b4f0c71d53f40b2b8dab8a6198ac586da
yushuhao <1241515599@qq.com>
2021-07-27 22:27:58 +0800

    Lowest Common Ancestor of a Binary Search Tree

---
+++ src/main/java/cn/meow/tree/lowestCommonAncestorBST/LowestCommonAncestorBST.java
@@ -0,0 +1,13 @@
+package cn.meow.tree.lowestCommonAncestorBST;
+
+import cn.meow.tree.been.TreeNode;
+
+public class LowestCommonAncestorBST {
+
+    /**
+     *
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+}
