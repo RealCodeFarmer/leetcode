e4eca074c59f9550f5d70e6922250932158f142e
yushuhao <1241515599@qq.com>
2021-08-02 23:17:34 +0800

    Lowest Common Ancestor of a Binary Tree

---
+++ src/main/java/cn/meow/tree/lowestCommonAncestor/LowestCommonAncestorBinaryTree2.java
@@ -0,0 +1,31 @@
+package cn.meow.tree.lowestCommonAncestor;
+
+import cn.meow.tree.been.TreeNode;
+
+public class LowestCommonAncestorBinaryTree2 {
+
+    private TreeNode ans = null;
+
+    /**
+     * 递归
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root, p, q);
+        return ans;
+    }
+
+    /**
+     * 子问题: 左右子树分别包含 p,q
+     */
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return false;
+        }
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || (root.val == p.val || root.val == q.val);
+    }
+}
