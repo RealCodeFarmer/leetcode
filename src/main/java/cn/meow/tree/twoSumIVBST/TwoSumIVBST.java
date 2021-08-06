38298d0a5f9a90db517d455ac5beb2f55aaf2db2
yushuhao <1241515599@qq.com>
2021-08-06 23:55:52 +0800

    Two Sum IV - Input is a BST

---
+++ src/main/java/cn/meow/tree/twoSumIVBST/TwoSumIVBST.java
@@ -0,0 +1,10 @@
+package cn.meow.tree.twoSumIVBST;
+
+import cn.meow.tree.been.TreeNode;
+
+public class TwoSumIVBST {
+
+    public boolean findTarget(TreeNode root, int k) {
+        return false;
+    }
+}
