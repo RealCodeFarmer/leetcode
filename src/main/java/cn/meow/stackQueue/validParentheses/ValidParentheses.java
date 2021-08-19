9b8857292ebc52b319b00d0183d90b85aeb64979
yushuhao <1241515599@qq.com>
2021-08-19 18:30:48 +0800

    Valid Parentheses

---
+++ src/main/java/cn/meow/stackQueue/validParentheses/ValidParentheses.java
@@ -0,0 +1,33 @@
+package cn.meow.stackQueue.validParentheses;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if (s == null || s.trim().length() == 0) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < chars.length; i++) {
+            char cur = chars[i];
+            if (cur == ')') {
+                if (stack.isEmpty() || !Character.valueOf('(').equals(stack.pop())) {
+                    return false;
+                }
+            } else if (cur == ']') {
+                if (stack.isEmpty() || !Character.valueOf('[').equals(stack.pop())) {
+                    return false;
+                }
+            } else if (cur == '}') {
+                if (stack.isEmpty() || !Character.valueOf('{').equals(stack.pop())) {
+                    return false;
+                }
+            } else {
+                stack.push(cur);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
