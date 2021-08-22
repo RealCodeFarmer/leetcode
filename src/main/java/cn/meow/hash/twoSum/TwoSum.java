a69808f9ccb5784fb6535677055c46c28dafb0e1
yushuhao <1241515599@qq.com>
2021-08-22 18:09:09 +0800

    Two Sum

---
+++ src/main/java/cn/meow/hash/twoSum/TwoSum.java
@@ -0,0 +1,23 @@
+package cn.meow.hash.twoSum;
+
+public class TwoSum {
+
+    /**
+     * 遍历
+     */
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int[] result = new int[2];
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
