768f1759872829337e771af3d2d1c562530afebf
yushuhao <1241515599@qq.com>
2021-08-24 10:42:09 +0800

    updaate

--- src/main/java/cn/meow/hash/containsDuplicate/ContainsDuplicate.java
+++ src/main/java/cn/meow/hash/containsDuplicate/ContainsDuplicate.java
@@ -1,8 +1,21 @@
 package cn.meow.hash.containsDuplicate;
 
+import java.util.HashMap;
+
 public class ContainsDuplicate {
 
     public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return true;
+            } else {
+                map.put(num, 1);
+            }
+        }
         return false;
     }
 }
