8b45df626f45d4ebef8f218656b43de31479dcbf
yushuhao <1241515599@qq.com>
2021-08-28 17:49:56 +0800

    Isomorphic Strings

---
+++ src/main/java/cn/meow/str/isomorphicStrings/IsomorphicStrings2.java
@@ -0,0 +1,21 @@
+package cn.meow.str.isomorphicStrings;
+
+public class IsomorphicStrings2 {
+
+    public boolean isIsomorphic(String s, String t) {
+        int[] preIndexOfS = new int[256];
+        int[] preIndexOfT = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+            //在不同位置出现过
+            if (preIndexOfS[sc] != preIndexOfT[tc]) {
+                return false;
+            }
+            preIndexOfS[sc] = i + 1;
+            preIndexOfT[tc] = i + 1;
+        }
+        return true;
+    }
+
+}
