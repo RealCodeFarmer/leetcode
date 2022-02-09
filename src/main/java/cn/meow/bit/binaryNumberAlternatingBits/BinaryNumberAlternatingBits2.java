609cd9beb3012e41accb20253cd7bee5e65b7dde
yush <1241515599@qq.com>
2022-02-09 17:48:43 +0800

    Binary Number with Alternating Bits

---
+++ src/main/java/cn/meow/bit/binaryNumberAlternatingBits/BinaryNumberAlternatingBits2.java
@@ -0,0 +1,12 @@
+package cn.meow.bit.binaryNumberAlternatingBits;
+
+public class BinaryNumberAlternatingBits2 {
+
+    /**
+     * 对于 1010 这种位级表示的数，把它向右移动 1 位得到 101，这两个数每个位都不同，因此异或得到的结果为 1111
+     */
+    public boolean hasAlternatingBits(int n) {
+        int a = (n ^ (n >> 1));
+        return (a & (a + 1)) == 0;
+    }
+}
