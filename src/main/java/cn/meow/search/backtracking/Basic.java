b87df472a3942663a6eab8e2aad5e6c6b5c07c4a
yush <1241515599@qq.com>
2022-03-05 22:53:30 +0800

    Word Search

---
+++ src/main/java/cn/meow/search/backtracking/Basic.java
@@ -0,0 +1,19 @@
+package cn.meow.search.backtracking;
+
+public class Basic {
+
+    /**
+     *  void backtracking(参数){
+     *      if(终止条件){
+     *          收集结果;
+     *          return;
+     *      }
+     *      for(集合元素){
+     *          处理节点;
+     *          递归操作;
+     *          回溯操作;
+     *      }
+     *      return;
+     *  }
+     */
+}
