c05f7e692ddfb732482fe6f7749a7f01367b2c3d
yush <1241515599@qq.com>
2022-03-09 08:54:04 +0800

    Permutations II

---
+++ src/main/java/cn/meow/search/backtracking/permutationsII/PermutationsII2.java
@@ -0,0 +1,44 @@
+package cn.meow.search.backtracking.permutationsII;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PermutationsII2 {
+
+    /**
+     * 在实现上，和 Permutations 不同的是要先排序，
+     * 然后在添加一个元素时，判断这个元素是否等于前一个元素，如果等于，并且前一个元素还未访问，那么就跳过这个元素
+     */
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> permutes = new ArrayList<>();
+        List<Integer> permuteList = new ArrayList<>();
+        // 排序
+        Arrays.sort(nums);
+        boolean[] hasVisited = new boolean[nums.length];
+        backtracking(permuteList, permutes, hasVisited, nums);
+        return permutes;
+    }
+
+    private void backtracking(List<Integer> permuteList, List<List<Integer>> permutes, boolean[] visited, final int[] nums) {
+        if (permuteList.size() == nums.length) {
+            permutes.add(new ArrayList<>(permuteList));
+            return;
+        }
+
+        for (int i = 0; i < visited.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1] && !visited[i - 1]) {
+                // 防止重复
+                continue;
+            }
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            permuteList.add(nums[i]);
+            backtracking(permuteList, permutes, visited, nums);
+            permuteList.remove(permuteList.size() - 1);
+            visited[i] = false;
+        }
+    }
+}
