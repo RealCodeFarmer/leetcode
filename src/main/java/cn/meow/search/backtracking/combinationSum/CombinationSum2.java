fdbe6b5a6e040922d34b702eb47e93baaef5f478
yush <1241515599@qq.com>
2022-03-11 15:54:47 +0800

    Combination Sum

---
+++ src/main/java/cn/meow/search/backtracking/combinationSum/CombinationSum2.java
@@ -0,0 +1,29 @@
+package cn.meow.search.backtracking.combinationSum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum2 {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        backtracking(new ArrayList<>(), combinations, 0, target, candidates);
+        return combinations;
+    }
+
+    private void backtracking(List<Integer> tempCombination, List<List<Integer>> combinations,
+                              int start, int target, final int[] candidates) {
+
+        if (target == 0) {
+            combinations.add(new ArrayList<>(tempCombination));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] <= target) {
+                tempCombination.add(candidates[i]);
+                backtracking(tempCombination, combinations, i, target - candidates[i], candidates);
+                tempCombination.remove(tempCombination.size() - 1);
+            }
+        }
+    }
+}
