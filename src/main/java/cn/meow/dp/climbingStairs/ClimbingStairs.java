3f4dd4002bebc56204886795b9f240e2ae74b4cc
yush <1241515599@qq.com>
2022-03-20 17:06:34 +0800

    Climbing Stairs

---
+++ src/main/java/cn/meow/dp/climbingStairs/ClimbingStairs.java
@@ -0,0 +1,25 @@
+package cn.meow.dp.climbingStairs;
+
+public class ClimbingStairs {
+
+    /**
+     * 超时
+     */
+    public int climbStairs(int n) {
+        int[] visit = new int[n + 1];
+        return climbStairs(n, visit);
+    }
+
+    private int climbStairs(int n, int[] visit) {
+        if (visit[n] > 0) {
+            return visit[n];
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs(n - 1, visit) + climbStairs(n - 2, visit);
+    }
+}
