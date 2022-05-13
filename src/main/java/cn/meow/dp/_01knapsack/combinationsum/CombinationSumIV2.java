054df29da5aaefe3bd1313fad048e1651657bf2b
yush <1241515599@qq.com>
2022-05-13 22:18:41 +0800

    Combination Sum IV

---
+++ src/main/java/cn/meow/dp/_01knapsack/combinationsum/CombinationSumIV2.java
@@ -0,0 +1,21 @@
+package cn.meow.dp._01knapsack.combinationsum;
+
+import java.util.Arrays;
+
+public class CombinationSumIV2 {
+
+    public int combinationSum4(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] maximum = new int[target + 1];
+        maximum[0] = 1;
+        Arrays.sort(nums);
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length && nums[j] <= i; j++) {
+                maximum[i] += maximum[i - nums[j]];
+            }
+        }
+        return maximum[target];
+    }
+}
